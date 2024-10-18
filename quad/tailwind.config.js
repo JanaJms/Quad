61b17b5eb88e67f820806a30ccfde6ef632eb427
JanaJms <janajamousdev@gmail.com>
2024-10-18 12:11:02 +0300

    Add colors defined to tailwind.config.js file

--- quad/tailwind.config.js
+++ quad/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default{
   content: [
-    "./index.html",             
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html", // Path to the main HTML file to be scanned for class names or directives             
+    "./src/**/*.{js,jsx,ts,tsx}", // Path to the source directory, including all JavaScript and TypeScript files
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors,
+      },
+    },
   },
   plugins: [],
 }
