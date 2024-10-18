a839062eeceb89b9737b050f2b01085f2b34e7c6
JanaJms <janajamousdev@gmail.com>
2024-10-18 11:15:18 +0300

    Edit Header.jsx

--- quad/src/App.jsx
+++ quad/src/App.jsx
@@ -3,17 +3,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
 import AppRouter from './router';
 
 function App() {
   return (
     <Router>
       <Header />
-      <AppRouter />
+      <AppRouter/>
       <Footer />
     </Router>
   );
--- quad/src/components/Header.jsx
+++ quad/src/components/Header.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from '../pages/Home';
+import Services from '../pages/Services';
+import Portfolio from '../pages/Portfolio';
+import Contact from '../pages/Contact';
 
 function Header() {
   return (
-    <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/services" element={<services />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-  </Router>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
