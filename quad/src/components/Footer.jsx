2a3d1c58421539785f0a44eb83d8fe9787ef04ba
JanaJms <janajamousdev@gmail.com>
2024-10-31 15:33:55 +0200

    Edit footer to be responsive

--- quad/src/components/Footer.jsx
+++ quad/src/components/Footer.jsx
@@ -4,23 +4,24 @@ import Logo from '../assets/react.svg';
 
 function Footer() {
   return (
-    <footer className="flex flex-col md:flex-row justify-between items-start px-40 pb-20 bg-white ">
+    <footer className="flex flex-col md:flex-row justify-between items-start px-6 md:px-20 lg:px-40 py-10 md:pb-20 bg-white text-lightPrimary">
 
       {/* Left Section - Logo and Description */}
-      <div className="flex-1 mb-11 md:mb-0">
-        <div className="flex items-center mb-12">
+      <div className="flex-1 mb-6 md:mb-0 text-center md:text-left">
+        <div className="flex justify-center md:justify-start items-center mb-6">
           <img src={Logo} alt="Logo" className="h-10 mr-2" />
         </div>
 
-        <p className="leading-7 tracking-wide text-left w-96 text-lightPrimary">
+        <p className="leading-7 tracking-wide max-w-xs md:max-w-sm mx-auto md:mx-0">
           Any business owner that is looking to increase their online sales or bring in more leads for their business.
         </p>
       </div>
 
       {/* Right Container - Links, Divider, and Copyright */}
-      <div className="flex-shrink-0 flex flex-col text-left md:w-1/3"> 
+      <div className="flex-shrink-0 flex flex-col items-center text-center md:text-left md:w-1/3  mx-auto mt-8 md:mt-0">
+        
         {/* Links Section */}
-        <div className="flex justify-between">
+        <div className="flex justify-between w-full md:text-left space-x-6 md:space-x-12">
           <div className="flex flex-col space-y-2">
             <Link to="/services" className="hover:text-accent">Services</Link>
             <Link to="/portfolio" className="hover:text-accent">Portfolio</Link>
@@ -34,10 +35,10 @@ function Footer() {
         </div>
 
         {/* Divider */}
-        <div className="w-full border-t text-lightSecondary my-4" />
+        <div className="w-full border-t border-lightSecondary my-6" />
 
         {/* Copyright Section */}
-        <div className="text-center md:text-left">
+        <div>
           <p>© All Rights Quad.</p>
         </div>
       </div>
